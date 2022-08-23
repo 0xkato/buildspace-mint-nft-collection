64475a8a08bd4c67945e85aa1357125b23d487ad
0xKato <emtolihi@gmail.com>
2022-08-23 16:55:30 +0200

    updated commit

--- contracts/MyEpicNFT.sol
+++ contracts/MyEpicNFT.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract MyEpicNFT {
-    constructor(){
+contract MyEpicNFT is ERC721URIStorage {
+
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+    constructor() ERC721 ("SquareNFT", "SQUARE"){
         console.log("we made our first smart contract");
     }
 
@@ -19,10 +23,10 @@ contract MyEpicNFT {
     _safeMint(msg.sender, newItemId);
 
     // Set the NFTs data.
-    _setTokenURI(newItemId, "brand new NFT POGCHAMP");
+    _setTokenURI(newItemId, "https://jsonkeeper.com/b/V8RB");
+    console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
 
     // Increment the counter for when the next NFT is minted.
     _tokenIds.increment();
   }
-}
 }
